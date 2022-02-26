282e350fc7f96691d78c20ceaf0887a6975dc2d3
User <user@mail.com>
2022-02-26 17:36:44 +0300

    error input

--- three_back_clcltr.py
+++ three_back_clcltr.py
@@ -126,6 +126,10 @@ class Collection_data():
 
     def __init__(self):
 
+        ##########ERROR_INPUT###########
+        self.list_for_error_input = []##
+        ################################
+
         self.list_all_simvols = []
         
         self.list_nubmer_and_brackets_open = [[]]
@@ -144,27 +148,32 @@ class Collection_data():
         return len(self.brackets_open)-len(self.brackets_close)
 
     def bracket_open(self):
-        self.list_all_simvols.append('(')
-    
-        if self.ask_level() == 0:
-            self.list_nubmer_and_brackets_open[0].append('(')
-        else:
-            self.list_nubmer_and_brackets_open[self.ask_level()][len(self.list_nubmer_and_brackets_open[self.ask_level()])-1].append('(')
+        if self.error_input('('):
+            self.list_all_simvols.append('(')
+            #############ERROR_INPUT##############
+            self.list_for_error_input.append('(')#
+            ######################################
+            if self.ask_level() == 0:
+                self.list_nubmer_and_brackets_open[0].append('(')
+            else:
+                self.list_nubmer_and_brackets_open[self.ask_level()][len(self.list_nubmer_and_brackets_open[self.ask_level()])-1].append('(')
+                
+            self.brackets_open.append('(')    
+            if len(self.list_nubmer_and_brackets_open) -1 < len(self.brackets_open) - len(self.brackets_close):
+                self.list_nubmer_and_brackets_open.append([])
+                self.list_level_miltiplication_division.append([])
+                self.list_level_plus_minus.append([])
+                self.list_level_all_operators.append([])
             
-        self.brackets_open.append('(')    
-        if len(self.list_nubmer_and_brackets_open) -1 < len(self.brackets_open) - len(self.brackets_close):
-            self.list_nubmer_and_brackets_open.append([])
-            self.list_level_miltiplication_division.append([])
-            self.list_level_plus_minus.append([])
-            self.list_level_all_operators.append([])
-        
-        self.list_nubmer_and_brackets_open[self.ask_level()].append([])             
-        self.list_level_all_operators[self.ask_level()].append([])
-        
+            self.list_nubmer_and_brackets_open[self.ask_level()].append([])             
+            self.list_level_all_operators[self.ask_level()].append([])
+            
     def bracket_close(self):
-        self.int_num()
-        self.brackets_close.append(')')
-        self.list_all_simvols.append(')')
+        if self.error_input(')'):
+            self.int_num()
+            self.brackets_close.append(')')
+            self.list_all_simvols.append(')')
+            self.list_for_error_input.append(')')
 
     def int_num(self):
         if self.simvols:
@@ -172,11 +181,13 @@ class Collection_data():
             for s in self.simvols:
                 snum += s
             if '.' in snum:
+                self.list_for_error_input.append(float(snum))
                 if self.ask_level():
                     self.list_nubmer_and_brackets_open[self.ask_level()][len(self.list_nubmer_and_brackets_open[self.ask_level()])-1].append(float(snum))
                 else:
                     self.list_nubmer_and_brackets_open[self.ask_level()].append(float(snum))
             else:
+                self.list_for_error_input.append(int(snum))
                 if self.ask_level():
                     self.list_nubmer_and_brackets_open[self.ask_level()][len(self.list_nubmer_and_brackets_open[self.ask_level()])-1].append(int(snum))
                 else:
@@ -186,81 +197,104 @@ class Collection_data():
 ######################################################################
 #############################POLE NUMBERS#############################
     def one(self):
-        self.simvols.append('1')
-        self.list_all_simvols.append('1')
+        if self.error_input('1'):
+            self.simvols.append('1')
+            self.list_all_simvols.append('1')
 
     def two(self):
-        self.simvols.append('2')
-        self.list_all_simvols.append('2')
+        if self.error_input('2'):
+            self.simvols.append('2')
+            self.list_all_simvols.append('2')
 
     def three(self):
-        self.simvols.append('3')
-        self.list_all_simvols.append('3')
+        if self.error_input('3'):
+            self.simvols.append('3')
+            self.list_all_simvols.append('3')
 
     def four(self):
-        self.simvols.append('4')
-        self.list_all_simvols.append('4')
+        if self.error_input('4'):
+            self.simvols.append('4')
+            self.list_all_simvols.append('4')
 
     def five(self):
-        self.simvols.append('5')
-        self.list_all_simvols.append('5')
+        if self.error_input('5'):
+            self.simvols.append('5')
+            self.list_all_simvols.append('5')
 
     def six(self):
-        self.simvols.append('6')
-        self.list_all_simvols.append('6')
+        if self.error_input('6'):
+            self.simvols.append('6')
+            self.list_all_simvols.append('6')
 
     def seven(self):
-        self.simvols.append('7')
-        self.list_all_simvols.append('7')
+        if self.error_input('7'):
+            self.simvols.append('7')
+            self.list_all_simvols.append('7')
 
     def eight(self):
-        self.simvols.append('8')
-        self.list_all_simvols.append('8')
+        if self.error_input('8'):    
+            self.simvols.append('8')
+            self.list_all_simvols.append('8')
 
     def nine(self):
-        self.simvols.append('9')
-        self.list_all_simvols.append('9')
+        if self.error_input('9'):
+            self.simvols.append('9')
+            self.list_all_simvols.append('9')
 
     def zero(self):
-        self.simvols.append('0')
-        self.list_all_simvols.append('0')
+        if self.error_input('0'):
+            self.simvols.append('0')
+            self.list_all_simvols.append('0')
 ######################################################################
 ############################POLE OPERATORS############################
     def plus(self):
-        self.list_all_simvols.append('+')
-        self.list_level_plus_minus[self.ask_level()].append('+')
-        if self.ask_level():
-            self.list_level_all_operators[self.ask_level()][len(self.list_level_all_operators[self.ask_level()])-1].append('+')
-        else:
-            self.list_level_all_operators[self.ask_level()].append('+')
-        self.int_num()
+        if self.error_input('+'):
+            self.list_all_simvols.append('+')
+            self.list_level_plus_minus[self.ask_level()].append('+')
+            if self.ask_level():
+                self.list_level_all_operators[self.ask_level()][len(self.list_level_all_operators[self.ask_level()])-1].append('+')
+            else:
+                self.list_level_all_operators[self.ask_level()].append('+')
+            self.int_num()
+            self.list_for_error_input.append('+')
 
     def minus(self):
-        self.list_all_simvols.append('-')
-        self.list_level_plus_minus[self.ask_level()].append('-')
-        if self.ask_level():
-            self.list_level_all_operators[self.ask_level()][len(self.list_level_all_operators[self.ask_level()])-1].append('-')
-        else:
-            self.list_level_all_operators[self.ask_level()].append('-')
-        self.int_num()
+        if self.error_input('-'):
+            self.list_all_simvols.append('-')
+            self.list_level_plus_minus[self.ask_level()].append('-')
+            if self.ask_level():
+                self.list_level_all_operators[self.ask_level()][len(self.list_level_all_operators[self.ask_level()])-1].append('-')
+            else:
+                self.list_level_all_operators[self.ask_level()].append('-')
+            self.int_num()
+            self.list_for_error_input.append('-')
 
     def miltiplication(self):
-        self.list_all_simvols.append('*')
-        self.list_level_miltiplication_division[self.ask_level()].append('*')
-        if self.ask_level():
-            self.list_level_all_operators[self.ask_level()][len(self.list_level_all_operators[self.ask_level()])-1].append('*')
-        else:
-            self.list_level_all_operators[self.ask_level()].append('*')
-        self.int_num()
+        if self.error_input('*'):
+            self.list_all_simvols.append('*')
+            self.list_level_miltiplication_division[self.ask_level()].append('*')
+            if self.ask_level():
+                self.list_level_all_operators[self.ask_level()][len(self.list_level_all_operators[self.ask_level()])-1].append('*')
+            else:
+                self.list_level_all_operators[self.ask_level()].append('*')
+            self.int_num()
+            self.list_for_error_input.append('*')
         
     def division(self):
-        self.list_all_simvols.append('/')
-        self.list_level_miltiplication_division[self.ask_level()].append('/')
-        if self.ask_level():
-            self.list_level_all_operators[self.ask_level()][len(self.list_level_all_operators[self.ask_level()])-1].append('/')
-        else:
-            self.list_level_all_operators[self.ask_level()].append('/')
-        self.int_num()    
+        if self.error_input('/'):
+            self.list_all_simvols.append('/')
+            self.list_level_miltiplication_division[self.ask_level()].append('/')
+            if self.ask_level():
+                self.list_level_all_operators[self.ask_level()][len(self.list_level_all_operators[self.ask_level()])-1].append('/')
+            else:
+                self.list_level_all_operators[self.ask_level()].append('/')
+            self.int_num()
+            self.list_for_error_input.append('/')
+
+    def capcha(self):
+        if self.error_input('.'):
+            self.simvols.append('.')
+            self.list_all_simvols.append('.')
 ######################################################################        
         
 
@@ -281,12 +315,52 @@ class Collection_data():
         simvols = ''
         for i in self.list_all_simvols:
             simvols += i
-            
+        print('simvols' , self.list_all_simvols)
         return simvols
 
+    def error_input(self, element):
 
+        resolution = False
+        
+        if element in '0 1 2 3 4 5 6 7 8 9':
+            if not len(self.list_for_error_input):
+                resolution = True
+            elif self.list_for_error_input[len(self.list_for_error_input)-1] in '+ - * /':
+                resolution = True
+            elif self.list_for_error_input[len(self.list_for_error_input)-1] == '(':
+                resolution = True
 
+        elif element == '(':
+            if not len(self.list_for_error_input):
+                resolution = True
 
+            elif self.list_for_error_input[len(self.list_for_error_input)-1] in '+ - * /':
+                resolution = True
+            
+            elif self.list_for_error_input[len(self.list_for_error_input)-1] == '(':
+                resolution = True
+
+        elif element == ')':
+            if len(self.list_for_error_input):
+                if len(self.brackets_open) - len(self.brackets_close) != 0 and self.list_for_error_input[len(self.list_for_error_input)-1] in '+ - * /' and self.simvols:
+                    resolution = True
+                elif self.list_for_error_input[len(self.list_for_error_input)-1] == ')' and len(self.brackets_open) - len(self.brackets_close) != 0:
+                    resolution = True
+                
+        elif element in  '+ - * /':
+            if not len(self.list_for_error_input) and self.simvols:
+                    resolution = True
+            
+            elif len(self.list_for_error_input):
+                if self.list_for_error_input[len(self.list_for_error_input)-1] == ')':
+                    resolution = True
+                elif self.simvols:
+                    resolution = True
+        elif element == '.':
+            if '.' not in self.simvols:
+                resolution = True
+                
+        return resolution 
 
 if __name__ == '__main__':
     print('You open modul back')
